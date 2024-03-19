2ccbf0e47171b9ce5547312ccdca79f14a57a7b9
Sushma Sahu <Sushma.Sahu@civica2.com>
2024-03-19 15:46:32 +0530

    project is added to github

---
+++ src/app/MyComponent/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo";
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent implements OnInit {
+  todos!: Todo[];
+  localItem: string;
+  constructor() {
+    this.localItem = localStorage.getItem("todos") || "null"
+    console.log("locslItem->",this.localItem);
+    if (this.localItem == null) {
+      this.todos = [];
+    }
+    else {
+      console.log("this is else block",this.localItem)
+       this.todos = JSON.parse(this.localItem)
+    }
+
+  }
+  ngOnInit(): void {
+    
+  }
+  deleteTodo(todo: Todo) {
+    console.log("this is delete method");
+    console.log(todo);
+    const index = this.todos.indexOf(todo)
+    this.todos.splice(index, 1)
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+  addTodo(todo: Todo) {
+    console.log(todo)
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+  toggelTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponent/todos/todos.component';
+import { TodosListComponent } from './MyComponent/todos-list/todos-list.component';
+import { AddTodoComponent } from './MyComponent/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { AboutComponent } from './MyComponent/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodosListComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
